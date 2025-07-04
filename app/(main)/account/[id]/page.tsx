4382d57a9aa64e49404b114f60fe5b94ba451685
Prithviraj Patil <prithvirajpatil64@gmail.com>
2025-07-04 12:34:41 +0530

    Prisma schema added. Basic routing added. Not Found page created.

---
+++ app/(main)/account/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AccountIDpage = () => {
+  return (
+    <div>AccountIDpage</div>
+  )
+}
+
+export default AccountIDpage
---
+++ app/(main)/transaction/create/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateTransactionPage = () => {
+  return (
+    <div>CreateTransactionPage</div>
+  )
+}
+
+export default CreateTransactionPage
